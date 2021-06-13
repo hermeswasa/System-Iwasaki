e476240796d5caff61bb057480894666c1c1ae1c
DaisukeAmano <amano.daisuke@astina.co>
2021-06-13 23:57:38 +0900

    refactoring

--- System_Iwasaki/System_Iwasaki.ino
+++ System_Iwasaki/System_Iwasaki.ino
@@ -3,7 +3,6 @@
 //Arduino側設定
 const int Servo_PIN = 6;
 const int AIN_PIN = 2;
-const int DIN_PIN = 7;
 
 //サーボモーター設定
 const int Pulse_MIN = 560;
@@ -15,13 +14,11 @@ Servo servo1;
 
 void setup() {
   servo1.attach(Servo_PIN);
-  pinMode(DIN_PIN, INPUT);
 }
 
 void loop() {
   int analogInput;
   int uSec;
-  int i = 0;
   
   while(1){
     analogInput = analogRead(AIN_PIN);
